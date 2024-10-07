20b74080f66b3e7e5bb74a8014fa68e5ff9a12b9
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-10-07 12:59:42 -0300

    add title

--- consumir-api.php
+++ consumir-api.php
@@ -9,31 +9,39 @@
  */
 
 get_header();?>
-<hr>
-
-<?php 
-
+<style>
+  .content{
+    border: 1px solid black;
+    margin: 10px;
+  }
+</style>
+<?php
 $urlEmbed = 'https://api.giphy.com/v1/gifs/trending?api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa';
-
     $responseEmbed = wp_remote_get( $urlEmbed, array(
         'method'      => 'GET'
         )
-    );
-     
-    if ( is_wp_error( $responseEmbed ) ) {
+    );     
+    if ( is_wp_error( $responseEmbed ) ) {      
         return false;
     } else {
         $rEmbed = wp_remote_retrieve_body( $responseEmbed );
         $result = json_decode($rEmbed, true);
     }
     ?>
+<pre>
 
-<?php
+  <?php 
+  // var_dump($result) 
+  ;?>
+</pre>
+  <?php
     foreach($result['data'] as $get_result){?>
-        <p><?php echo $get_result['title'] .'<br>' ;?></p>
-        <img src="<?php echo $get_result['images']['480w_still']['url'] .'<br>' ;?>" width="30px" height="30">
+    <div class="content">
+      <p>Titulo: <?php echo $get_result['title'] ;?></p>
+      <p class="">Id: <?php echo $get_result['id'] ;?></p>
+      <img src="<?php echo $get_result['images']['480w_still']['url'];?>" width="90px" height="90">
+    </div>
   <?php }?>
 
 
-<hr>
 <?php  get_footer();?>
