d114cbcf9a7a8ea0c7d055ac707c7388099afad3
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-10-07 10:18:38 -0300

    update

---
+++ consumir-api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: consumir api
+ * Template Post Type: post, page, event
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ *
+ * @package Prodesp
+ */
+
+get_header();?>
+<hr>
+
+
+
+<hr>
+<?php  get_footer();?>
